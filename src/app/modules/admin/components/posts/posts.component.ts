3ccf0efdc47858da60503fbd6b93a6d4e4c9816c
May Via <vi-113@yandex.ru>
2023-11-21 21:28:15 +0400

    refactoring
    (cherry picked from commit 7eefa31b8984f1786cc00fa898da9881b9fa232b)

--- src/app/modules/admin/components/posts/posts.component.ts
+++ src/app/modules/admin/components/posts/posts.component.ts
@@ -2,8 +2,6 @@ import { Post } from '../../../../core/interfaces/post';
 import { PostsService } from 'src/app/core/services/posts.service';
 import { Component, OnInit } from '@angular/core';
 import { delay, tap } from 'rxjs/operators';
-import { Router } from '@angular/router';
-import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
@@ -20,9 +18,7 @@ export class PostsComponent implements OnInit {
   dataSourcePage: Post [] = [];
 
   constructor(
-    private readonly router: Router,
-    private readonly postsService: PostsService,
-    private readonly cdr: ChangeDetectorRef
+    private readonly postsService: PostsService
   ) {
     this.page = 1;
   }
@@ -40,17 +36,15 @@ export class PostsComponent implements OnInit {
   }
 
   remove(id: string): void {
-    this.postsService.remove(id).subscribe(() => {
-      this.dataSource = this.dataSource.filter((post: Post) => post.id !== id);
-
-      // Recalculate pagination after removing the post
-      const startIndex = this.pageSize * (this.page - 1);
-      this.dataSourcePage = this.dataSource.slice(startIndex, startIndex + this.pageSize);
-
-      this.cdr.detectChanges(); // Trigger change detection
-    });
+    this.postsService.remove(id).pipe(
+      tap(() => {
+          this.dataSource = this.dataSource.filter((post: Post) => post.id !== id);
+          this.dataSourcePage = this.dataSource.filter((post, i) => i < this.pageSize * this.page && i >=
+            this.pageSize * (this.page - 1));
+        }
+      )
+    ).subscribe();
   }
-
 
   onPageChanged(pageNumber: number): void  {
     console.log( pageNumber);
